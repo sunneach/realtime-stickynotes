29003e00841ab5b4c254395e87a66a2603e8cc0a
suneach <76492000+suneach@users.noreply.github.com>
2021-09-22 10:24:01 -0400

    Copy javascript libs from jQuery-pure-libs

--- makeit.bat
+++ makeit.bat
@@ -1,4 +1,9 @@
 @set MOCHIWEB_BIN_DIR=..\mochiweb\ebin
 @if not exist %MOCHIWEB_BIN_DIR% mkdir %MOCHIWEB_BIN_DIR%
 @if not exist ebin mkdir ebin
+
+rem copy Javascript stash
+@set JQUERY_PURE_LIBS=..\jQuery-pure-libs
+@if not exist priv\js\pure.js  copy %JQUERY_PURE_LIBS%\js\*.* priv\js
+
 erl -make
